02a5e9b6869263388453e3e4f8ba83ed7a776faa
rheadsz <63584256+rheadsz@users.noreply.github.com>
2020-09-06 01:41:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,42 +6,50 @@ app = Flask(__name__)
 Electricity = [{'COMPLAINT_ID': "0",
           'complainant': "Mr.Danish Sait",
           'address': " Near Infosys, Urwa Mangalore",
+	  'Contact no.': "897654326",
           'description': "Wrong and disproportionate bills"},
 
          {'COMPLAINT_ID': "1",
           'complainant': "Aditi Hegde",
           'address': "Sai Baba Temple, Chilimbi, Mangalore",
+	  'Contact no.': "45678907",
           'description': "Very old bills without notice"}
         ]
 
 Road = [{'COMPLAINT_ID': "0",
           'complainant': "Meaford Pinto",
           'address': "Near Cambridge School, Neeramarga, Mangalore",
+	  'contact no.': "1234567890",
           'description': "Potholes and broken roads"},
 
          {'COMPLAINT_ID': "1",
           'complainant': "Athul SK",
           'address': "Majila Jeppu,Near St.Josephs Seminary, Mangalore",
+	  'contact no.': "43267889087",
           'description': "No proper footpath for the pedestrians"}]
 
 Sanitary = [{'COMPLAINT_ID': "0",
           'complainant': "Manish shetty",
           'address': "Nandigudda Mangalore",
+	  'contact no.': "4532167890",
           'description': "No proper garbage disposal facility"},
 
          {'COMPLAINT_ID': "1",
           'complainant': "Aster Machado",
           'address': "Casia High School, Monkey Stand, Mangalore",
+	  'contact no.': "5432178906",
           'description': "Open drains "}]
 
 Water = [{'COMPLAINT_ID': "0",
           'complainant': "Disha Attavar",
           'address': "5th Cross, Urwa Mangalore",
+	  'contact no.': "6754327896",
           'description': "Bills for others' connection"},
 
          {'COMPLAINT_ID': "1",
           'complainant': "Meghana Shetty",
           'address': "Bejai New Road, Opp Bejai Market,Bejai, Mangalore",
+	  'contact no.': "7654327890",
           'description': "Disconnection without notice and reason"}]
 
 
